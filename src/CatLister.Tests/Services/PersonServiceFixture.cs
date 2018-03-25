b840404edce6f76dbc84155052eb58f1a191e469
Daniel Simpson <daniel_simpson1988@hotmail.com>
2018-03-25 17:23:23 +1100

    Add final test

--- src/CatLister.Tests/Services/PersonServiceFixture.cs
+++ src/CatLister.Tests/Services/PersonServiceFixture.cs
@@ -3,6 +3,9 @@ using NSubstitute;
 using Xunit;
 using CatLister.Services;
 using System.Threading.Tasks;
+using System.Reflection;
+using System.IO;
+using System.Linq;
 
 namespace CatLister.Tests.Services
 {
@@ -30,6 +33,40 @@ namespace CatLister.Tests.Services
             catsByGenderOwner.Should().NotBeNull();
             catsByGenderOwner.CatsWithMaleOwners.Should().BeEmpty();
             catsByGenderOwner.CatsWithFemaleOwners.Should().BeEmpty();
+        }
+
+        [Fact]
+        public async Task Index_ReturnsCorrectSplit_WhenUpstreamDataIsValid()
+        {
+            var testData = GetEmbeddedResourceContents("people.json");
+            mockHttpClient.GetAsync().ReturnsForAnyArgs(_ =>
+            {
+                return Task.FromResult(testData);
+            });
+
+            var personService = new PersonService(mockHttpClient);
+            var catsByGenderOwner = await personService.GetCatsByOwnerGenderAsync();
+
+            catsByGenderOwner.Should().NotBeNull();
+            catsByGenderOwner.CatsWithMaleOwners.Count().Should().Be(4);
+            catsByGenderOwner.CatsWithMaleOwners.First().Name.Should().Be("Garfield");
+            catsByGenderOwner.CatsWithMaleOwners.Last().Name.Should().Be("Tom");
+
+            catsByGenderOwner.CatsWithFemaleOwners.Count().Should().Be(3);
+            catsByGenderOwner.CatsWithFemaleOwners.First().Name.Should().Be("Garfield");
+            catsByGenderOwner.CatsWithFemaleOwners.Last().Name.Should().Be("Tabby");
+        }
+
+        private string GetEmbeddedResourceContents(string filename)
+        {
+            var assembly = Assembly.GetExecutingAssembly();
+            var resourceName = $"CatLister.Tests.{filename}";
+
+            using (Stream stream = assembly.GetManifestResourceStream(resourceName))
+            using (StreamReader reader = new StreamReader(stream))
+            {
+                return reader.ReadToEnd();
+            }
         }
     }
 }
